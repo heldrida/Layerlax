3e1445238fa0c3d13813dae58d53d4f9c445bc4d
Helder <heldrida@gmail.com>
2015-01-21 18:37:30 +0000

    work in progress, trying to limit the scale on one of the axis to scale the layer with the circles

--- js/Layerlax.js
+++ js/Layerlax.js
@@ -25,8 +25,6 @@
 
   Layerlax.prototype.onMouseMove = function (event) {
 
-    console.log('onMouseMove()');
-
     this.bounds = this.element.getBoundingClientRect();
  
     // mouse cursor position
@@ -37,29 +35,65 @@
     this.w = this.bounds.width;
     this.h = this.bounds.height;
 
+    console.log('this.bounds: ', this.bounds);
+
     // center
     this.cx = this.w / 2;
-    this.cy = this.y / 2;
+    this.cy = this.h / 2;
 
     // amount from center
     this.ix = (event.clientX - this.x - this.cx) / this.cx;
     this.iy = (event.clientY - this.y - this.cy) / this.cy;
 
-    // clip to element bounds
-    this.ix = Math.max(this.ix, -1);
-    this.ix = Math.min(this.ix, 1);
-    this.iy = Math.max(this.ix, -1);
-    this.iy = Math.min(this.ix, 1);
+    // clip to element bounds, but keep all values positive,
+    // also set boundaries of element set to 0.6/1.2
+    //this.ix = Math.abs(this.ix);
+    //this.iy = Math.abs(this.iy);
+
+    /*
+    this.ix = Math.max(this.ix, 0);
+    this.ix = Math.min(this.ix, 5);
+    this.iy = Math.max(this.iy, 0);
+    this.iy = Math.min(this.iy, 5);
+    */
 
   };
 
   Layerlax.prototype.onAnimationFrame = function () {
 
-    console.log('onAnimationFrame()');
+    for (var i = 0; i < this.layers.length; i++) {
 
-    console.log('this.ix: ', this.ix);
+      this.setStyle(this.layers[i], this.ix, this.iy);
+
+    }
 
     this.requestAnimationFrame = requestAnimationFrame(this.onAnimationFrame.bind(this));
+
+  };
+
+  Layerlax.prototype.setStyle = function (element, x, y) {
+    
+    var val = (parseFloat(x) > 0 ? x : 1),
+        resistance = element.getAttribute('data-depth'),
+        scale = element.getAttribute('data-scale');
+ 
+        console.log('scale: ', scale);
+
+        val = val * resistance;
+        x = x * resistance;
+        y = y * resistance;
+
+        // element.style.webkitTransform = 'scale(' + val + ')';
+        
+        if (!scale) {
+
+          element.style.webkitTransform = 'translate3d(' + x + '%, ' + y + '%, 0)';
+
+        } else {
+
+          element.style.webkitTransform = 'scale(' + x + ')';
+
+        }
 
   };
 
