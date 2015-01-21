045598ed54f7e05c7443a596dccbcad5ca70c8e5
Helder <helder.correia@mrm-meteorite.com>
2015-01-21 16:40:09 +0000

    small amends, fixing the instance name, now not clashing with plugin name. changed on mouse move

--- js/Layerlax.js
+++ js/Layerlax.js
@@ -5,6 +5,10 @@
 
   function Layerlax(element) {
 
+    // DOM Context
+    this.element = element;
+    this.layers = element.getElementsByClassName('layer');
+
     // Element
     this.bounds = null;
     this.x = 0;
@@ -21,34 +25,56 @@
 
   Layerlax.prototype.onMouseMove = function (event) {
 
-  	this.bounds = this.element.getBoundingClientRect();
+    console.log('onMouseMove()');
+
+    this.bounds = this.element.getBoundingClientRect();
  
-  	// mouse cursor position
-  	this.x = this.bounds.left;  	
-  	this.y = this.bounds.top;
+    // mouse cursor position
+    this.x = this.bounds.left;    
+    this.y = this.bounds.top;
+    
+    // size
+    this.w = this.bounds.width;
+    this.h = this.bounds.height;
 
-  	// size
-  	this.w = this.bounds.width;
-  	this.h = this.bounds.height;
+    // center
+    this.cx = this.w / 2;
+    this.cy = this.y / 2;
 
-  	// center
-  	this.cx = this.w / 2;
-  	this.cy = this.y / 2;
+    // amount from center
+    this.ix = (event.clientX - this.x - this.cx) / this.cx;
+    this.iy = (event.clientY - this.y - this.cy) / this.cy;
+
+    // clip to element bounds
+    this.ix = Math.max(this.ix, -1);
+    this.ix = Math.min(this.ix, 1);
+    this.iy = Math.max(this.ix, -1);
+    this.iy = Math.min(this.ix, 1);
+
+  };
+
+  Layerlax.prototype.onAnimationFrame = function () {
+
+    console.log('onAnimationFrame()');
+
+    console.log('this.ix: ', this.ix);
+
+    this.requestAnimationFrame = requestAnimationFrame(this.onAnimationFrame.bind(this));
 
   };
 
   Layerlax.prototype.enable = function() {
-    window.addEventListener('mousemove', this.onMouseMove);
-    requestAnimationFrame(this.onAnimationFrame);
+    window.addEventListener('mousemove', this.onMouseMove.bind(this));
+    requestAnimationFrame(this.onAnimationFrame.bind(this));
   };
 
   Layerlax.prototype.initialise = function() {
 
-  	this.enable();
+    this.enable();
 
   };
 
   // Expose Parallax
-  window.Layerlax = Layerlax;
+  window['Layerlax'] = Layerlax;
 
 })(window, document);
