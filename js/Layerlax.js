acdb16686816462508a121b79ac14a12953a1951
Helder <heldrida@gmail.com>
2015-01-22 12:23:59 +0000

    small amends. created destroy method. a few tweaks on the transition time

--- js/Layerlax.js
+++ js/Layerlax.js
@@ -18,6 +18,9 @@
     this.ix = 0;
     this.iy = 0;
 
+    // timeout
+    this.timeout = null;
+
     // Initialise
     this.initialise();
 
@@ -25,35 +28,42 @@
 
   Layerlax.prototype.onMouseMove = function (event) {
 
-    this.bounds = this.element.getBoundingClientRect();
- 
-    // mouse cursor position
-    this.x = this.bounds.left;    
-    this.y = this.bounds.top;
-    
-    // size
-    this.w = this.bounds.width;
-    this.h = this.bounds.height;
+    clearInterval(this.timeout);
 
-    // center
-    this.cx = this.w / 2;
-    this.cy = this.h / 2;
+    this.timeout = setTimeout(function () {
 
-    // amount from center
-    this.ix = (event.clientX - this.x - this.cx) / this.cx;
-    this.iy = (event.clientY - this.y - this.cy) / this.cy;
+      this.bounds = this.element.getBoundingClientRect();
+   
+      // mouse cursor position
+      this.x = this.bounds.left;    
+      this.y = this.bounds.top;
+      
+      // size
+      this.w = this.bounds.width;
+      this.h = this.bounds.height;
 
-    // clip to element bounds, but keep all values positive,
-    // also set boundaries of element set to 0.6/1.2
-    //this.ix = Math.abs(this.ix);
-    //this.iy = Math.abs(this.iy);
+      // center
+      this.cx = this.w / 2;
+      this.cy = this.h / 2;
 
-    /*
-    this.ix = Math.max(this.ix, 0);
-    this.ix = Math.min(this.ix, 5);
-    this.iy = Math.max(this.iy, 0);
-    this.iy = Math.min(this.iy, 5);
-    */
+      // amount from center
+      this.ix = (event.clientX - this.x - this.cx) / this.cx;
+      this.iy = (event.clientY - this.y - this.cy) / this.cy;
+
+      // clip to element bounds, but keep all values positive,
+      // also set boundaries of element set to 0.6/1.2
+      //this.ix = Math.abs(this.ix);
+      //this.iy = Math.abs(this.iy);
+
+      /*
+      this.ix = Math.max(this.ix, 0);
+      this.ix = Math.min(this.ix, 5);
+      this.iy = Math.max(this.iy, 0);
+      this.iy = Math.min(this.iy, 5);
+      */
+
+
+    }.bind(this), 100);
 
   };
 
@@ -63,9 +73,9 @@
 
       var z =  Math.abs(this.ix);
       z = z + 0.5;
-      z = Math.min(z, 1.25);
-      z = Math.max(z, 1);
-      console.log('after z: ', z);
+      z = Math.min(z, 1.1);
+      z = Math.max(z, 0.9);
+ 
       this.setStyle(this.layers[i], this.ix, this.iy, z);
 
     }
@@ -98,12 +108,20 @@
 
   };
 
-  Layerlax.prototype.enable = function() {
+  Layerlax.prototype.enable = function () {
     window.addEventListener('mousemove', this.onMouseMove.bind(this));
     requestAnimationFrame(this.onAnimationFrame.bind(this));
   };
 
-  Layerlax.prototype.initialise = function() {
+  Layerlax.prototype.destroy = function () {
+
+    // DOM Context
+    delete this.element;
+    delete this.layers;
+
+  };
+
+  Layerlax.prototype.initialise = function () {
 
     this.enable();
 
