e486bcaeca649a956369c03dce1defbeba8cfcf5
Helder <heldrida@gmail.com>
2015-01-23 11:35:51 +0000

    different scalable layers. invert feature added.

--- js/Layerlax.js
+++ js/Layerlax.js
@@ -13,19 +13,21 @@
     this.bounds = null;
     this.x = 0;
     this.y = 0;
+    this.z = 0;
     this.w = 0;
     this.h = 0;
     this.ix = 0;
     this.iy = 0;
+    this.ax = 0;
+    this.ay = 0;
 
     // velocity
     this.vx = 0;
     this.vy = 0;
-    this.vz = 0;
+    this.vz = {}; // map, because scalable elements have different values
 
     // friction
     this.friction = 0.01;
-
 
     // timeout
     this.timeout = null;
@@ -34,7 +36,13 @@
     this.lock = false;
 
     // throttle delay
-    this.throttleDelay = 12;
+    this.throttleDelay = 12; // in ms (timer, for setPositionScaling function call. Optimization!)
+
+    // inverse X/Y
+    this.inverse = true;
+
+    // anchor center
+    this.anchorCenter = false;
 
     // Initialise
     this.initialise();
@@ -65,6 +73,10 @@
       this.ix = (event.clientX - this.x - this.cx) / this.cx;
       this.iy = (event.clientY - this.y - this.cy) / this.cy;
 
+      // amount from top/left
+      this.ax = event.clientX / this.w;
+      this.ay = event.clientY / this.h;
+
       // clip to element bounds, but keep all values positive,
       // also set boundaries of element set to 0.6/1.2
       //this.ix = Math.abs(this.ix);
@@ -87,11 +99,15 @@
     for (var i = 0; i < this.layers.length; i++) {
 
       var z =  Math.abs(this.ix);
-      z = z + 0.5;
-      z = Math.min(z, 1);
-      z = Math.max(z, 0.75);
+      // z = z + 0.5;
+      // z = Math.min(z, 1.12);
+      // z = Math.max(z, 1);
 
-      this.setStyle(this.layers[i], this.ix, this.iy, z);
+      if (this.anchorCenter !== true) {
+        this.setStyle(this.layers[i], this.ax, this.ay, z);
+      } else {
+        this.setStyle(this.layers[i], this.ix, this.iy, z);        
+      }
 
     }
 
@@ -100,17 +116,39 @@
   };
 
   Layerlax.prototype.setStyle = function (element, x, y, z) {
-    
+
     var val = (parseFloat(x) > 0 ? x : 1),
         depth = element.getAttribute('data-depth'),
         scale = element.getAttribute('data-scale');
 
         this.vx += (x - this.vx) * this.friction;
         this.vy += (y - this.vy) *  this.friction;
-        this.vz += (z - this.vz) * this.friction;
+
+        if (scale) {
+
+          var layer = 'layer_' + element.getAttribute('data-layer'),
+              zoomIn = parseFloat(element.getAttribute('data-zoom-in')),
+              zoomOut = parseFloat(element.getAttribute('data-zoom-out'));
+
+          if (!this.vz.hasOwnProperty(layer)) {
+            this.vz[layer] = 0;
+          }
+
+          // set Z boundaries
+          z = Math.min(z, zoomOut);
+          z = Math.max(z, zoomIn);
+
+          this.vz[layer] += (z - this.vz[layer]) * this.friction;
+
+        }
 
         this.xOffset = this.vx * depth; 
         this.yOffset = this.vy * depth;
+
+        if (this.inverse) {
+          this.xOffset *= -1;
+          this.yOffset *= -1;
+        }
 
         if (!scale) {
 
@@ -122,14 +160,13 @@
 
         } else {
 
-          element.style.webkitTransform = 'scale(' + this.vz + ')';
-          element.style.MozTransform = 'scale(' + this.vz + ')';
-          element.style.msTransform = 'scale(' + this.vz + ')';
-          element.style.oTransform = 'scale(' + this.vz + ')';
-          element.style.transform = 'scale(' + this.vz + ')';
+          element.style.webkitTransform = 'scale(' + this.vz[layer] + ') translate3d(' + this.xOffset + '%, ' + this.yOffset + '%, 0)';
+          element.style.MozTransform = 'scale(' + this.vz[layer] + ') translate3d(' + this.xOffset + '%, ' + this.yOffset + '%, 0)';
+          element.style.msTransform = 'scale(' + this.vz[layer] + ') translate3d(' + this.xOffset + '%, ' + this.yOffset + '%, 0)';
+          element.style.oTransform = 'scale(' + this.vz[layer] + ') translate3d(' + this.xOffset + '%, ' + this.yOffset + '%, 0)';
+          element.style.transform = 'scale(' + this.vz[layer] + ') translate3d(' + this.xOffset + '%, ' + this.yOffset + '%, 0)';
 
         }
-
 
   };
 
